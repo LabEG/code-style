706a1232947d9c0c5e328458a8e7e93ae931eb84
Labutin Evgeniy <labeg@mail.ru>
2021-06-17 17:45:23 +0300

    update eslint rule, remove postcss config and tsconfig for pwa

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,121 +1,69 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2020: true
-    },
-    extends: ["eslint:all", "plugin:react/all", "plugin:@typescript-eslint/all"],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         project: "tsconfig.json",
+        sourceType: "module",
         ecmaFeatures: {
             jsx: true
         }
     },
     plugins: ["react", "@typescript-eslint"],
+    extends: [
+        "eslint:all",
+        "plugin:react/all",
+        "plugin:@typescript-eslint/all"
+    ],
+    root: true,
+    env: {
+        browser: true,
+        es2021: true
+    },
+    ignorePatterns: ["node_modules/*"],
     settings: {
         react: {
             version: "detect"
         }
     },
     rules: {
-        "linebreak-style": ["error", "windows"],
-        "sort-imports": "off",
-        "object-curly-spacing": ["error", "always"],
-        "max-len": ["error", { code: 140, comments: 140 }],
-        "padded-blocks": ["error", { classes: "always", blocks: "never", switches: "never" }],
-        "lines-between-class-members": "off",
-        "function-call-argument-newline": ["error", "consistent"],
-        "quote-props": ["error", "as-needed"],
-        "sort-keys": "off",
-        "max-classes-per-file": ["error", 5],
-        "class-methods-use-this": "off",
-        "object-property-newline": ["error", { allowAllPropertiesOnSameLine: true }],
-        "max-statements": ["error", 30],
-        "no-negated-condition": "off",
-        "one-var": ["error", "never"],
-        "no-ternary": "off",
-        "multiline-ternary": ["error", "always-multiline"],
-        "id-length": "off",
-        "camelcase": "off", // починить код и вернуть правило
-        "max-lines-per-function": ["error", { "max": 300 }],
-        "array-element-newline": ["error", "consistent"],
-        "lines-around-comment": ["error", { allowBlockStart: true }],
-        "operator-linebreak": ["error", "after"],
-        "no-return-assign": "off",
-        "capitalized-comments": "off",
-        "no-void": "off",
-        "max-params": ["error", { "max": 4 }],
-        "no-bitwise": "off",
-        "prefer-named-capture-group": "off",
-        "dot-location": ["error", "property"],
-        "no-inline-comments": "off",
-        "line-comment-position": "off",
-        "yoda": ["error", "never", { onlyEquality: true }],
-        "no-case-declarations": "off",
+        "sort-imports": "off", // need found sorter
+        "max-len": ["error", { code: 140, comments: 140 }], // for modern screens
+        "padded-blocks": ["error", { classes: "always", blocks: "never", switches: "never" }], // more nice
+        "function-call-argument-newline": ["error", "consistent"], // maybe later?
+        "quote-props": ["error", "as-needed"], // more nice
+        "sort-keys": "off", // more nice
+        "class-methods-use-this": "off", // false positives
+        "one-var": ["error", "never"], // more nice
+        "no-ternary": "off", // more nice
+        "multiline-ternary": ["error", "always-multiline"], // more nice
+        "array-element-newline": ["error", "consistent"], // more nice
+        "operator-linebreak": ["error", "after"], // more nice
+        "no-void": "off", // strange rule
+        "max-params": ["error", { "max": 4 }], // maybe later?
+        "no-bitwise": "off", // needed
+        "prefer-named-capture-group": "off", // maybe later?
+        "dot-location": ["error", "property"], // more nice
+        "no-inline-comments": "off", // maybe later?
+        "line-comment-position": "off", // maybe later?
 
-        "react/jsx-closing-bracket-location": ["error", "after-props"],
-        "react/jsx-first-prop-new-line": ["error", "never"],
-        "react/jsx-no-literals": "off",
-        "react/jsx-no-bind": "off", // maybe deprecate later?
-        "react/jsx-max-props-per-line": ["error", { maximum: 2 }],
-        "react/jsx-sort-props": ["error", { noSortAlphabetically: true }],
-        "react/jsx-max-depth": ["error", { "max": 10 }],
-        "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
-        "react/no-array-index-key": "off",
-        "react/destructuring-assignment": "off",
-        "react/no-adjacent-inline-elements": "off", // crash linting
-        "react/function-component-definition": "off",
-        "react/prop-types": "off",
-        "react/no-danger": "off",
-        "react/sort-comp": "off",
-        "react/no-multi-comp": ["error", { ignoreStateless: true }],
-        "react/forbid-component-props": "off",
+        "react/jsx-no-literals": "off", // broken rule, not work with ??
+        "react/jsx-max-depth": ["error", { "max": 10 }], // to small by default
+        "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }], // for typescript
+        "react/function-component-definition": ["error", { "namedComponents": "arrow-function" }], // same as eslint func-styles
+        "react/no-danger": "off", // for insert html
+        "react/forbid-component-props": "off", // conflict with styled-components
+        "react/jsx-uses-react": "off", // https://ru.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
+        "react/react-in-jsx-scope": "off", // https://ru.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
+        "react/sort-comp": "off", // need write priority
 
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/no-unused-vars": ["error", { ignoreArgsIfArgsAfterAreUsed: true }],
-        "@typescript-eslint/no-inferrable-types": "off",
-        "@typescript-eslint/ban-types": ["error", {
-            "types": {
-                "{}": "Use classes instead",
-                "String": {
-                    "message": "Use string instead",
-                    "fixWith": "string"
-                },
-                "Number": {
-                    "message": "Use number instead",
-                    "fixWith": "number"
-                },
-                "Boolean": {
-                    "message": "Use boolean instead",
-                    "fixWith": "boolean"
-                }
-            },
-            "extendDefaults": false
-        }],
-        "@typescript-eslint/space-before-function-paren": ["error", "never"],
-        "@typescript-eslint/no-use-before-define": ["error", { classes: false }],
-        "@typescript-eslint/strict-boolean-expressions": "off",
-        "@typescript-eslint/explicit-member-accessibility": ["error"],
+        "@typescript-eslint/no-inferrable-types": "off", // need for reflection
         "@typescript-eslint/no-magic-numbers": "off",
-        "@typescript-eslint/restrict-template-expressions": ["error", { allowNumber: true, allowNullable: true }],
-        "@typescript-eslint/prefer-readonly-parameter-types": "off",
-        "@typescript-eslint/no-floating-promises": "off",
-        "@typescript-eslint/no-extra-parens": "off",
-        "@typescript-eslint/explicit-member-accessibility": ["error", { overrides: { constructors: "off" } }],
-        "@typescript-eslint/promise-function-async": "off",
-        "@typescript-eslint/no-misused-promises": ["error", { "checksVoidReturn": false }],
-        "@typescript-eslint/no-unnecessary-condition": "off",
-        "@typescript-eslint/return-await": ["error", "always"],
-        "@typescript-eslint/no-unused-vars-experimental": ["error", { ignoreArgsIfArgsAfterAreUsed: true }],
-        "@typescript-eslint/no-unused-vars": "off",      
-        "@typescript-eslint/no-invalid-void-type": "off", // new rule, check later
-        "@typescript-eslint/member-ordering": "off", // current eslint version bug, enable after fix
-        "@typescript-eslint/lines-between-class-members": "off",
-        "@typescript-eslint/naming-convention": "off",
-        "@typescript-eslint/no-implicit-any-catch": "off", // typescript don't accept catch typings
-        "@typescript-eslint/consistent-type-imports": "off",
-        "@typescript-eslint/no-confusing-void-expression": "off",
-        "@typescript-eslint/object-curly-spacing": "off",
-        "@typescript-eslint/no-type-alias": "off"
+        "@typescript-eslint/prefer-readonly-parameter-types": "off", // wrong immutable undestand
+        "@typescript-eslint/no-floating-promises": "off", // block async promises
+        "@typescript-eslint/no-extra-parens": "off", // conflict with react best practise in jsx
+        "@typescript-eslint/promise-function-async": "off", // more nice
+        "@typescript-eslint/no-misused-promises": ["error", { "checksVoidReturn": false }], // more nice
+        "@typescript-eslint/naming-convention": "off", // bad with react func components
+        "@typescript-eslint/no-confusing-void-expression": "off", // more nice
+        "@typescript-eslint/member-ordering": "off", // need correct priority
     }
 };
